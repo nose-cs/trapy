2c8840696cf38bc621fb1e14a048e8dc766ba162
Anabel Benítez González <anabelbenitezglez@gmail.com>
2024-01-02 18:40:02 -0500

    refactor

--- trapy/port_manager.py
+++ trapy/port_manager.py
@@ -6,22 +6,22 @@ class ConnException(Exception):
 
 
 def bind(port):
-    with open("./files/ports.json") as fp:
+    with open("./ports.json") as fp:
         occupied_ports = json.load(fp)
     if port in occupied_ports:
         raise ConnException("port " + str(port) + " is occupied")
-    with open("./files/ports.json", "w") as fp:
+    with open("./ports.json", "w") as fp:
         json.dump(occupied_ports + [port], fp=fp, ensure_ascii=False, indent=2)
 
 
 def close_port(port):
-    with open("./files/ports.json") as fp:
+    with open("./ports.json") as fp:
         occupied_ports = json.load(fp)
 
     if port not in occupied_ports:
         raise ConnException("port " + str(port) + " is not occupied")
 
-    with open("./files/ports.json", "w") as fp:
+    with open("./ports.json", "w") as fp:
         json.dump(
             [p for p in occupied_ports if p != port],
             fp=fp,
@@ -31,14 +31,14 @@ def close_port(port):
 
 
 def get_port():
-    with open("./files/ports.json") as fp:
+    with open("./ports.json") as fp:
         occupied_ports = json.load(fp)
 
     for port in range(int(2 ** 16)):
         if port in occupied_ports:
             continue
 
-        with open("./files/ports.json", "w") as fp:
+        with open("./ports.json", "w") as fp:
             json.dump(occupied_ports + [port], fp=fp, ensure_ascii=False,
                       indent=2)
         return port
